e8be52142ff8540ffa80d9943f506b2092d9da33
kris <kris@doctoralliance.com>
2017-11-17 10:16:23 -0600

    introduced before and after class annotations

--- src/main/java/FlightBookingTest.java
+++ src/main/java/FlightBookingTest.java
@@ -2,6 +2,8 @@ import GenericLib.Driver;
 import pageObjectLib.FlightBook;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class FlightBookingTest {
@@ -11,18 +13,25 @@ public class FlightBookingTest {
 	//FlightBook reference declaration
 	FlightBook flightBookPage;
 
+	@BeforeClass
+	public void configBeforClass(){
+		//browser call
+		driver = Driver.setBrowser();
+		
+		//pagefactory initiation
+		flightBookPage = PageFactory.initElements(driver, FlightBook.class);
+		
+	}
 
     @Test
     public void testThatResultsAppearForAOneWayJourney() {
-    	
-    	//browser call
-    	driver = Driver.setBrowser();
-    	
-    	//pagefactory initiation
-    	flightBookPage = PageFactory.initElements(driver, FlightBook.class);
-    	
-        
+ 
     	driver.get("https://www.cleartrip.com/");
     	flightBookPage.bookFlight();
+    }
+    
+    @AfterClass
+	public void configAfterClass(){
+    	driver.quit();
     }
 }
